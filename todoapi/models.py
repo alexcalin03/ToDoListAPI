e2c5f65fdc23a436d8d7966bfab93212025e7f67
alexcalin03 <156395503+alexcalin03@users.noreply.github.com>
2024-12-24 02:14:56 +0200

    commit everything

--- todoapi/models.py
+++ todoapi/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Project(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=100)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
 
     def __str__(self):
         return self.name
@@ -15,6 +17,7 @@ class Todo(models.Model):
         due_date=models.DateField(blank=True, null=True)
         priority=models.CharField(max_length=10, blank=True, null=True)
         project=models.ForeignKey(Project, on_delete=models.CASCADE)
+        user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
 
         def __str__(self):
             return self.title
--- todoapi/urls.py
+++ todoapi/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from .views import project_list, todo_list, register_user,logout_user
+from .views import project_list, todo_list, register_user,logout_user, user_projects, user_todos, create_project, create_todo, delete_project, delete_todo,update_project, update_todo
+
 
 urlpatterns = [
     path('projects/', project_list, name='project_list'),
     path('todos/', todo_list,name='todo_list'),
     path('register/', register_user, name='register_user'),
     path('logout/', logout_user, name='logout_user'),
+    path('user_projects/', user_projects, name='user_projects'),
+    path('user_todos/', user_todos, name='user_todos'),
+    path('create_project/', create_project, name='create_project'),
+    path('create_todo/', create_todo, name='create_todo'),
+    path('delete_project/<int:project_id>/', delete_project, name='delete_project'),
+    path('delete_todo/<int:todo_id>/', delete_todo, name='delete_todo'),
+    path('update_project/<int:project_id>/', update_project, name='update_project'),
+    path('update_todo/<int:todo_id>/', update_todo, name='update_todo'),
 ]
 
 
