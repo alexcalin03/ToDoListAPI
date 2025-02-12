cb97317afafaeaefd3ab5671d7d3779f2d2b5cea
alexcalin03 <156395503+alexcalin03@users.noreply.github.com>
2025-02-13 00:15:05 +0200

    Reset repo to match local version

--- todoapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Project, Todo, Notification
-
-admin.site.register(Project)
-admin.site.register(Todo)
-admin.site.register(Notification)
-
-# Register your models here.
